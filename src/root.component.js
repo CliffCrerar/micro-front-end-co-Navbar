60717352dab7c4d701b21ba81e0088ea11d9543b
CliffCrerar <cliff.crerar@gmail.com>
2020-08-09 04:54:09 +0200

    created submodulees

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import "./root.component.css";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  // return <section>{props.name} is mounted!</section>;
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/page-one" className="link">
+          Page 1
+        </Link>
+        <Link to="/page-two" className="link">
+          Page 2
+        </Link>
+      </nav>
+    </BrowserRouter>
+  );
 }
